a1ba113958cf030abfbb088ec0f1b4790f9a1369
spatel025 <spatel.097.25@gmail.com>
2017-12-02 18:23:25 -0500

    Fifth Commit - Recipe-list-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a Test', 'https://www.ndtv.com/cooks/images/kerala.chicken.roast%20%281%29.jpg')
+    new Recipe('First Recipe', 'This is a First Recipe', 'https://www.ndtv.com/cooks/images/kerala.chicken.roast%20%281%29.jpg'),
+    new Recipe('Second Recipe', 'This is a Second Recipe', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
